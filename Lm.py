d762748f0b28c7209fdada9d289e3e4e01f412a8
ViktoriaBarakhovskaya <vika7234705@mail.ru>
2022-12-10 00:37:33 +0300

    3 lesson

---
+++ Lm.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+df = pd.read_csv('D:\\ML and Data Science\\UNZIP_ME_FOR_NOTEBOOKS_ML_RUS_V1\\03-Pandas\\tips.csv')
+print(df.columns)
+print(df['total_bill'].sum())
+
+
