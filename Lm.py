25739c55b7c22f5698082e53fef5affc75190dcc
ViktoriaBarakhovskaya <vika7234705@mail.ru>
2022-12-10 00:40:18 +0300

    add function

--- Lm.py
+++ Lm.py
@@ -3,5 +3,6 @@ import pandas as pd
 df = pd.read_csv('D:\\ML and Data Science\\UNZIP_ME_FOR_NOTEBOOKS_ML_RUS_V1\\03-Pandas\\tips.csv')
 print(df.columns)
 print(df['total_bill'].sum())
+print(df['tip'].mean())
 
 
